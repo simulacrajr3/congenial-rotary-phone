457abc522e9c0f26260411433f65a76a248e75fa
simulacrajr3 <74381552+simulacrajr3@users.noreply.github.com>
2020-11-29 20:52:24 +0100

    Update nospoilersjavascript.js

--- nospoilersjavascript.js
+++ nospoilersjavascript.js
@@ -1,54 +1,23 @@
 <script type="text/javascript">
-function hide_show_table(col_name)
-{
- var checkbox_val=document.getElementById(col_name).value;
- if(checkbox_val=="hide")
- {
-  var all_col=document.getElementsByClassName(col_name);
-  for(var i=0;i<all_col.length;i++)
-  {
-   all_col[i].style.display="visible";
-  }
-  document.getElementById(col_name+"_head").style.display="visible";
-  document.getElementById(col_name).value="show";
- }
 	
- else
- {
-  var all_col=document.getElementsByClassName(col_name);
-  for(var i=0;i<all_col.length;i++)
-  {
-   all_col[i].style.display="table-cell";
-  }
-  document.getElementById(col_name+"_head").style.display="table-cell";
-  document.getElementById(col_name).value="hide";
- }
-}
-</script>
+	
+	
+    <!-- get Matchday -->
+  //<![CDATA[
 
-  <!-- get Matchday -->
+  function filterTable() {
+    // Variables
+    let dropdown, table, rows, cells, Matchday, filter;
+    dropdown = document.getElementById("MatchdayDropdown");
+    table = document.getElementById("myTable");
+    rows = table.getElementsByTagName("tr");
+    filter = dropdown.value;
 
-<script>
-  <script >
-function filterTable() {
-  // Variables
-  let dropdown, table, rows, cells, Matchday, filter;
-  dropdown = document.getElementById("MatchdayDropdown");
-  table = document.getElementById("myTable");
-  rows = table.getElementsByTagName("tr");
-  filter = dropdown.value;
-
-  // Loops through rows and hides those with countries that don't match the filter
-  for (let row of rows) { // `for...of` loops through the NodeList
-    cells = row.getElementsByTagName("td");
-    Matchday = cells[1] || null; // gets the 2nd `td` or nothing
-    // if the filter is set to 'All', or this is the header row, or 2nd `td` text matches filter
-    if (filter === "All" || !Matchday || (filter === Matchday.textContent)) {
-      row.style.display = ""; // shows this row
-    }
-    else {
-      row.style.display = "none"; // hides this row
-    }
-  }
-}
+    // Loops through rows and hides those with countries that don't match the filter
+    for (let row of rows) { // `for...of` loops through the NodeList
+      cells = row.getElementsByTagName("td");
+      Matchday = cells[1] || null; // gets the 2nd `td` or nothing
+      // if the filter is set to 'All', or this is the header row, or 2nd `td` text matches filter
+      
+//]]>
 </script>
